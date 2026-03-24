6791fb59f002c094b6f35fd776c7ca40d16d41a6
Aarushi Choradia <109821064+icedrakon17@users.noreply.github.com>
2026-03-23 20:26:39 -0700

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Aarushi Choradia" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi! My name is Aarushi, and I am a sophomore in the Bay Area. I am a front percussionist in marching band and I play high school basketball. I have been programming basic projects for a long time, and I am hoping to expand my scope as a programmer with this course. Nice to meet all of you!")
 if __name__ == "__main__":
     main()
